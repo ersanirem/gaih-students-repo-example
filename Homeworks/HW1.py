59651d9809fc87d4dc35f19540ad19b7619b3ac6
ersanirem <81179572+ersanirem@users.noreply.github.com>
2021-03-25 17:41:25 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -2,4 +2,15 @@
 The system learns its past experiences with the model it has developed using its data.
 Ability to use this knowledge on new data without the need for reprogramming for future tasks.
 
-#Question 
+#Question 2; What are the differences between Supervised and Unsupervised Learning? Specify example 3 algorithms for each of these. 
+ Supervised Learnig is a approach that here the program is given labeled input data and the expected output results.But in unsupervised learning, 
+we have unlabeled training data.
+ For example;
+  1- Image classification. We can use supervised learning to check if it is a dog or a cat in the picture.
+  2- Predicting next year's sales volume
+  3- Checking an email if it is spam or not spam
+  For unsupervised learning examples;
+  1-Clustering DNA patterns to analyze evolutionary biology.
+  2-Recommender systems - giving better Amazon purchase suggestions or Netflix movie matches
+  3-Medical imaging - for distinguishing between different kinds of tissues
+  
