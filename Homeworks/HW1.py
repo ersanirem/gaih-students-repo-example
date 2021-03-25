de34ca51714fe69199f844d21fad5dcea775d5c3
ersanirem <81179572+ersanirem@users.noreply.github.com>
2021-03-25 17:56:53 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -2,6 +2,8 @@
 The system learns its past experiences with the model it has developed using its data.
 Ability to use this knowledge on new data without the need for reprogramming for future tasks.
 
+
+
 #Question 2; What are the differences between Supervised and Unsupervised Learning? Specify example 3 algorithms for each of these. 
  Supervised Learnig is a approach that here the program is given labeled input data and the expected output results.But in unsupervised learning, 
 we have unlabeled training data.
@@ -14,3 +16,18 @@ we have unlabeled training data.
   2-Recommender systems - giving better Amazon purchase suggestions or Netflix movie matches
   3-Medical imaging - for distinguishing between different kinds of tissues
   
+  
+  
+ #Question 3; What are the test and validation set, and why would you want to use them?
+ – Training set: A set of examples used for learning, that is to fit the parameters of the classifier.
+ –  Validation set: A set of examples used to tune the parameters of a classifier, for example to choose the number of hidden units in a neural network.
+ It is usually used for parameter selection and to avoid overfitting
+ – Test set: The sample of data used to provide an unbiased evaluation of a final model fit on the training dataset.
+  
+In order to reach a good and effective algorithm, to make accurate and true predictions we should use validation and test test.
+Because we can avoid overfitting with use of validation test.And with test set we can see the accuracy of our model and see how good it works.
+
+
+
+#Question 4; What are the main preprocessing steps? Explain them in detail. Why we need to prepare our data? 
+ 
