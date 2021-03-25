fd0bf3465fed427c4cc3440a301255028ced5847
ersanirem <81179572+ersanirem@users.noreply.github.com>
2021-03-25 18:42:09 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -2,3 +2,54 @@
 
 
 
+# Check duplicate values and missing data
+
+
+
+
+# Visualize data for each feature (pairplot,distplot)
+
+
+
+
+# Draw correlation matrix
+
+
+
+# Drop correlated features (check correlation matrix)
+
+
+
+
+
+# Handle outliers (you can use IsolationForest)
+
+
+
+
+
+# Normalize data
+
+
+# Split dataset into train and test set
+
+
+
+
+# Import ridge and lasso models from sklearn
+
+
+
+
+# Define 5 different alpha values for lasso and fit them. Print their R^2 sore on both
+# train and test.
+
+
+
+
+# Define 5 different alpha values for Ridg and fit them. Print their R^2 sore on both
+# train and test.
+
+
+
+# Make comment about results. Print best models coefficient.
